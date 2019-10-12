f5019fc5cf210d9fe2086f32219368460a52e801
efe <jimmyfallon717@gmail.com>
2019-10-12 12:55:01 +0300

    server.js edited.

--- server.js
+++ server.js
@@ -14,9 +14,16 @@ app.get('/', function (req, res) {
 
 io.sockets.on('connection', function (socket) {
     connections.push(socket);
-    console.log('Connected: %s sockets connected', connections.length)
+    console.log('Connected: %s sockets connected', connections.length);
 
     // disconnect
-    connections.splice(connections.indexOf(socket), 1);
-    console.log('Disconnected: %s sockets connected', connections.length);
+    socket.on('disconnect', function (data) {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected', connections.length);
+    });
+
+    // Send Message
+    socket.on('send message', function (data) {
+        io.sockets.emit('new message', { msg: data });
+    });
 });
